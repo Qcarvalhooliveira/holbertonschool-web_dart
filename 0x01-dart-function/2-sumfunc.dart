d338b7dfa8a8d73ca6a8a694874517023d61e091
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-06-27 12:06:36 +0200

    Add/Sub

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,14 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+String showFunc(int a, int b) {
+    String addition = 'Add ${a} + ${b} = ${add(a, b)}';
+    String subtraction = 'Sub ${a} - ${b} = ${sub(a, b)}';
+
+  return '${addition}\n ${subtraction}';
+}
