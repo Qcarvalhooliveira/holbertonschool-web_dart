ea2d1b558b9945b0986143dd468a170553bb0491
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-06-29 15:42:43 +0200

    Fixing Json to User

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -26,6 +26,6 @@ class User {
 
   @override
   String toString() {
-    return 'User(id: $id, name: $name, age: $age, height: $height)';
+    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})';
   }
 }
