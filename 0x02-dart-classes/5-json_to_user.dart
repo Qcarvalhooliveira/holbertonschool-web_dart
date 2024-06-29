621a2d1f5e1a1c527ff7897cf9041971afbfe19e
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-06-29 15:06:25 +0200

    Json to User

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,31 @@
+class User {
+  int id;
+  String name;
+  int age;
+  double height;
+
+  User({required this.id, required this.name, required this.age, required this.height});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $id, name: $name, age: $age, height: $height)';
+  }
+}
